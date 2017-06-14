ae98750bd8855c0a2f5be9624deac7e7e4ec7d34
WetzelVictor <wetzel.victor@gmail.com>
2017-06-14 16:14:23 +0200

    almost working: some formants are still missing. Artifact should be corrected too.

--- formantTracking.m
+++ formantTracking.m
@@ -5,20 +5,33 @@
 % Fe - sampling frequency (Hz)
 
 function [ F ] = formantTracking( A, Fe )
+
+% find roots
 rts = roots(A);
 rts = rts(imag(rts) >= 0);
+
+% converts in angles
 angz = atan2(imag(rts),real(rts));
+
+% converts to frequencues
 [frqs, indices] = sort(angz.*(Fe/(2*pi)));
+
+% computes bandwidth
 bw = -1/2 * (Fe/(2*pi))*log(abs(rts(indices)));
 
+% initialize formants storing array
+formants = zeros(size(A));
+
+% loop through formants
 nn = 1;
 for kk = 1:length(frqs)
-    if (frqs(kk) > 90 && bw(kk) <400)
+    if (frqs(kk) > 90 && frqs(kk) < 5000 && bw(kk)< 1000)
         formants(nn) = frqs(kk);
         nn = nn+1;
     end
 end
 
-F = [frqs ; zeros(length(A) - length(frqs), 1)];
+% output results in a correct format
+F = [formants' ; zeros(length(A) - length(formants), 1)];
 end
 
--- main.m
+++ main.m
@@ -1,49 +1,34 @@
 close all; clear all; 
 
 %% LOAD AUDIO 
-
-% vowel 'a'
-[sigA, Fe] = audioread('audio/a.aif');
-sigA = 0.9*sigA/max(abs(sigA)); % normalize
-preemph = [1 0.63];
-sigA = filter(1,preemph,sigA);
-
-% vowel 'i'
-[sigI, ~] = audioread('audio/i.aif');
-sigI = 0.9*sigI/max(abs(sigI)); % normalize
-sigI = filter(1,preemph,sigI);
-
 % random audio signal
-[sig, ~] = audioread('audio/full-sentence.wav');
+[sig, Fe] = audioread('audio/i.aif');
 sig = 0.9*sig/max(abs(sig)); % normalize
-sig = filter(1,preemph,sig);
+
+% preemph = [1 0.63];
+% sig = filter(1,preemph,sig);
 
 %% GLOBAL VARIABLES
-
 % Various
 Te = 1/ Fe;
 fmax = Fe / 2;
-Ni = length(sigI);
-Na = length(sigA);
-Nfft = 1024;
 
 % ANALYSIS
+N = length(sig);
 Nwin = 256;
 win = hamming(Nwin, 'periodic');
-Nover = floor(0.5 *Nwin);
-p = 10; % number of LPC poles 
+over = 0.5;
+Nover = floor(over *Nwin);
+Nframes = floor(N/(Nwin*over)) - 2;
+Nfft = 1024;
+p = 25; % number of LPC poles 
 
-% interpolation parameters
-tInterp = 5; % time of interpolation (s)
-nInterp = floor(tInterp * Fe);
-Nframes = floor(nInterp / Nwin); % number of frames
-
-% PLOT VECTORS
-t = [0 : nInterp] * Te;
-f = [-fmax : Fe/nInterp : fmax];
+% VECTORS PLOT
+t = [0:Nframes] * Te;
+f = [-fmax : Fe/Nfft : fmax];
 
 % Spectrogram
-[a.A, a.E, a.K, a.F] = spectroFormant(sigA, p, Fe, win, Nover, Nfft);
-[i.A, i.E, i.K, i.F] = spectroFormant(sigI, p, Fe, win, Nover, Nfft);
-
 [A, E, K, F] = spectroFormant(sig, p, Fe, win, Nover, Nfft);
+
+figure;
+plot(t, F')
