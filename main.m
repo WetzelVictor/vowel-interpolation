99892447a240463f2785bbf4571ace8973e8a534
WetzelVictor <wetzel.victor@gmail.com>
2017-06-26 11:38:22 +0200

    cleaning

--- main.m
+++ main.m
@@ -1,18 +1,15 @@
 %%% VICTOR WETZEL
 % vowel-interpolation
 % GitHub: https://github.com/WetzelVictor/vowel-interpolation
-
 clear all; close all;
+
 
 %% BASIC NFO
 Nwin = 1024;
 win = hamming(Nwin, 'periodic');
 over = 0.5;
-p = 1 + floor( 44100/1200 );
-% p = 25;
 
 %% LOAD AUDIO 
-
 % vowel 1
 [v1.sig, Fe] = audioread('audio/i-flat.wav');
 v1.sig = v1.sig(:,1); % to mono
@@ -23,35 +20,29 @@ v1.sig = v1.sig / (max(abs(v1.sig)));
 v2.sig = v2.sig(:,1); % to mono
 v2.sig = v2.sig / (max(abs(v2.sig)));
 
+% uncomment to displya first few periods of both signal
+% checkPhaseAlignement.m
 
 %% BASIC INFOS
 v1.N = length(v1.sig);
 v2.N = length(v2.sig);
 
-% % % FIGURE visualizing phase alignement % % %
-figure
-firstSamples = floor( Fe/110 );
-plot(v2.sig([ 1:firstSamples ] + 20000));
-hold on
-plot(v1.sig([1:firstSamples] + 20000));
-grid on
-title('Visualizing phase alignement')
-xlabel('Samples')
-ylabel('Amplitude')
-legend('vowel a','vowel i')
-
 %% ANALYSIS
+% Defining the order of the analysis
+p = 1 + floor(Fe/1200); % replace denominator by 1kHz for a male voice
+
+% Analysis for both vowel signals
 [v1.A, v1.K, v1.res] = analysis(v1.sig, Fe, p, win, over);
 [v2.A, v2.K, v2.res] = analysis(v2.sig, Fe, p, win, over);
 
 
-%% INTERPOLATION:
-
-% Source (residual)
+%% INTERPOLATION
+% Source/residual 
+%   - Shortest signal determines interpolated residual (iRes)
 iRes = interpSource(v1.res, v2.res);
 [~, Nframes] = size(stackOLA(iRes, win, over));
 
-% Filter
+% Interpolating Reflection coefficient
 Kc1 = v1.K(:, 110);
 Kc2 = v2.K(:, 100);
 [A, K, P] = interpolateTubeSize( [Kc1 Kc2], Nframes, false);
@@ -59,5 +50,9 @@ Kc2 = v2.K(:, 100);
 %% RESYNTHESIS
 synth = myFilter(iRes, 1, A, win);
 
+%% PLAYBACK
+% Uncomment to play resynthesized samples
 % soundsc(synth, Fe);
-% audiowrite(output,synth,Fe)
+
+% Uncomment to save sound
+% audiowrite('output/synth.wav',synth,Fe)
