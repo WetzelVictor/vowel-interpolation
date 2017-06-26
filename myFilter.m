c7310959a095ac6b0fe9843c694efea29d9cca56
WetzelVictor <wetzel.victor@gmail.com>
2017-06-26 10:00:23 +0200

    myFilter ok

--- myFilter.m
+++ myFilter.m
@@ -12,6 +12,7 @@
 function Sig = myFilter(sig, B, A, win)
 
 %% DEFAULT
+% if B or A is equal to one, then we create an empty array that is only filled with ones
 if B == 1,
   [~, Nframes] = size(A);
   B = ones(1, Nframes);
@@ -24,21 +25,40 @@ end
 N = length(sig);
 Nwin = length(win);
 
+%% INIT
+Sig = zeros(N, 1); % output signal
+flagA = 0; % top index for the given frame
+flagB = 0; % bottom index ...............
+
+
 %% LOOP
 % first step: stores initial status for the filter
-[Sig(1:Nwin,1), zf] = filter(B(:,1),A(:,1), sig(1:Nwin, 1) );
-Sig = zeros(N, 1);
+
+% computing indexes for each frame
+flagA = 1;
+flagB = flagA + Nwin;
+
+% filtering first frame
+[Sig(1:Nwin,1), zf] = filter(B(:,1), A(:,1), sig(1:Nwin, 1) );
 
 % other steps
 for i = 2: Nframes,
-  flagA = Nwin * i + 1;
-  flagB = flagA + Nwin;
-
+  % computing frame's indexes
+  flagA = flagB + 1;
+  flagB = flagA + Nwin ;
+  
+  % If we are at the end of the signal
   if flagB > N,
     flagB = N;
+    break 
   end
 
+  % filters the given frame
   [Sig(flagA:flagB,1), zf]= filter(B(:,i),A(:,i), sig(flagA:flagB,1), zf);
+
+  %%% END OF THE LOOP %%%
 end
 
+
+%%% THE END %%%
 end
